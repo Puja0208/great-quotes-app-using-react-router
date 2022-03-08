2ac7d7e33cf6379139b239643c695c61a2b35b88
puja.rani <puja.rani@ril.com>
2022-03-08 18:09:23 +0530

    feat: Define routes to different pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import { Route, Switch, Redirect } from "react-router-dom";
+
+import AllQuotes from "./pages/AllQuotes";
+import QuoteDetail from "./pages/QuoteDetail";
+import NewQuote from "./pages/NewQuote";
 function App() {
   return (
-    <div>
-
-    </div>
+    <Switch>
+      <Route exact path="/">
+        <Redirect to="/quotes" />
+      </Route>
+      <Route exact path="/quotes">
+        <AllQuotes />
+      </Route>
+      <Route path="/quotes/:quoteId">
+        <QuoteDetail />
+      </Route>
+      <Route path="/new-quote">
+        <NewQuote />
+      </Route>
+    </Switch>
   );
 }
 
