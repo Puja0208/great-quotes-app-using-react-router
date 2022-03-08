984599d870f5da84327d99e36c831c72e877bd81
puja.rani <puja.rani@ril.com>
2022-03-08 18:27:30 +0530

    feat: Fill dummy quotes and render in all quotes component

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,5 +1,21 @@
+import QuoteList from "../components/quotes/QuoteList";
+import QuoteDetail from "./QuoteDetail";
+
+const DUMMY_QUOTES = [
+  {
+    id: "q1",
+    author: "Max",
+    text: "Learning React is fun",
+  },
+  {
+    id: "q2",
+    author: "John",
+    text: "Learning React is great",
+  },
+];
+
 const AllQuotes = () => {
-  return <h1>All Quotes</h1>;
+  return <QuoteList quotes={DUMMY_QUOTES} />;
 };
 
 export default AllQuotes;
